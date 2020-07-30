532f6ba32330897b73a3299f17a350c672157c24
Ubpa <641614112@qq.com>
2020-07-30 22:27:27 +0800

    delete Reflection

--- cmake/UbpaAutoRefl.cmake
+++
@@ -1,7 +0,0 @@
-function(Ubpa_AutoRefl input output)
-  add_custom_command(
-    OUTPUT ${output}
-	COMMAND Ubpa::UDP_app_AutoRefl ARGS ${input} ${output}
-	DEPENDS Ubpa::UDP_app_AutoRefl
-  )
-endfunction()
